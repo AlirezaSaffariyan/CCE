ba46fffbeb4ceeb5e3030e66007b720e6b5f1cc6
ars <ar.saffariyan@gmail.com>
2025-05-27 11:08:13 +0330

    Add basic databse async session management and base model

---
+++ app/db/session.py
@@ -0,0 +1,8 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+
+from app.config import get_settings
+
+settings = get_settings()
+
+engine = create_async_engine(settings.database_url, echo=True)
+async_session = async_sessionmaker(engine, expire_on_commit=False)
